663667ca6eeedb7db6ce74c89400e2d5f6f8baea
Francois <rabanel.francois@gmail.com>
2017-11-27 17:07:22 +0100

    index.html.erb: div header + card content

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,4 @@ Rails.application.routes.draw do
   resources :spaceships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-resources :spaceships
-
 end
