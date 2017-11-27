82cc014375021970f476f0ddc9ab5aceecdc4c17
Nicolas Amiel <n.amiel.andersen@gmail.com>
2017-11-27 15:14:09 +0100

    few changes on view/controller spaceship

--- app/controllers/spaceships_controller.rb
+++ app/controllers/spaceships_controller.rb
@@ -1,2 +1,10 @@
 class SpaceshipsController < ApplicationController
+
+  def index
+    @spaceships = Spaceship.all
+  end
+
+  def show
+    @spaceship = Spaceship.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'spaceships#index'
+
+  resources :spaceships
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
