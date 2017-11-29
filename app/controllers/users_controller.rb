d3a998ce02831ac9ed09aea31ba51d75808dc54a
Nicolas Amiel <n.amiel.andersen@gmail.com>
2017-11-29 13:55:39 +0100

    multiple changes to hangar and other adjustments to models and dependencies

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def spaceship_params
+    params.require(:user).permit(:email)
+  end
+end
